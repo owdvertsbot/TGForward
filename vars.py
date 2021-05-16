43f9987df79eda26537ff62a03b2cc89f60e9ee6
Anjana Madushanka <anjanama2007@gmail.com>
2021-05-16 19:10:56 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,6 +4,6 @@ class config(object):
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
-    FROM_CHANNEL_ID = os.environ.get("FROM_CHANNEL_ID", None)
-    TO_CHANNEL_ID = os.environ.get("TO_CHANNEL_ID", None)
+    FROM_CHANNEL_ID = int(os.environ.get("FROM_CHANNEL_ID", None))
+    TO_CHANNEL_ID = int(os.environ.get("TO_CHANNEL_ID", None))
     CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
