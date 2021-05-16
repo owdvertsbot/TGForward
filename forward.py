5e0d5786ed1e9f671e44fe744fadd378d1c6f2f8
Anjana Madushanka <anjanama2007@gmail.com>
2021-05-16 18:56:44 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -12,17 +12,22 @@ from vars import config
 
 api_id = config.API_ID
 api_hash = config.API_HASH
+from_chat = config.FROM_CHANNEL_ID
+to_chat = config.TO_CHANNEL_ID
+custom_caption = config.CUSTOM_CAPTION
 bot = TelegramClient(StringSession(config.STRING_SESSION), api_id, api_hash)
 
 bot.start()
 
+print("Please wait starting forwarding")
+print("Start auto forwarding from " + from_chat + " to " + to_chat)
+
 async def forward():
-  async for msg in bot.iter_messages(config.FROM_CHANNEL_ID, reverse=True, filter=InputMessagesFilterDocument):
+  async for msg in bot.iter_messages(from_chat, reverse=True, filter=InputMessagesFilterDocument):
     try:
       await asyncio.sleep(1)
-      k = await client.send_file(config.TO_CHANNEL_ID, file=msg.media, caption=config.CUSTOM_CAPTION)
+      k = await client.send_file(to_chat, file=msg.media, caption=custom_caption)
     except FloodError as e:
-      
       asyncio.sleep(e.seconds)
-bot.loop.run_until_complete(clone())
+bot.loop.run_until_complete(forward())
 bot.run_until_disconnected()
