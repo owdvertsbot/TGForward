30439f6fef15ce71b03d3bc99491e5b2b0bad7ef
Anjana Madushanka <anjanama2007@gmail.com>
2021-05-16 18:45:00 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -11,8 +11,8 @@ from telethon.errors import FloodError
 from vars import config
 
 api_id = config.API_ID
-api_hash = "{config.API_HASH}"
-bot = TelegramClient(StringSession("{config.STRING_SESSION}"), api_id, api_hash)
+api_hash = config.API_HASH
+bot = TelegramClient(StringSession(config.STRING_SESSION), api_id, api_hash)
 
 bot.start()
 
@@ -20,8 +20,9 @@ async def forward():
   async for msg in bot.iter_messages(config.FROM_CHANNEL_ID, reverse=True, filter=InputMessagesFilterDocument):
     try:
       await asyncio.sleep(1)
-      k = await client.send_file(config.TO_CHANNEL_ID, file=msg.media, caption="{config.CUSTOM_CAPTION}")
+      k = await client.send_file(config.TO_CHANNEL_ID, file=msg.media, caption=config.CUSTOM_CAPTION)
     except FloodError as e:
+      
       asyncio.sleep(e.seconds)
 bot.loop.run_until_complete(clone())
 bot.run_until_disconnected()
