393c5da297a270c6e9baa04cd1c30958d3fc951d
Anjana Madushanka <71401231+Anjana-Ma@users.noreply.github.com>
2021-05-17 23:23:13 +0530

    Update start.py

--- start.py
+++ start.py
@@ -14,6 +14,6 @@ AnjanaMa = Client('AnjanaMa',
                   api_id=Config.API_ID,
                   api_hash=Config.API_HASH,
                   bot_token=Config.BOT_TOKEN,
-                  plugins=dict(root="plugins"))
+                  root="plugins")
 
 AnjanaMa.run()
